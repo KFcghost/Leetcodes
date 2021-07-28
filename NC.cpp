b3603d45993efed476fadf02435d3997ec1b3140
KFcghost <66289994+KFcghost@users.noreply.github.com>
2021-07-28 23:38:32 +0800

    7-28 final

--- NC.cpp
+++ NC.cpp
@@ -2,7 +2,6 @@
  * @Description: LEETCODE
  * @Author: Aspirin
  * @Date: 2021-07-28 23:21:04
- * @LastEditTime: 2021-07-28 23:21:04
+ * @LastEditTime: 2021-07-28 23:34:17
  * @LastEditors: Aspirin
  */
-test
