e7312d7557de6c436a8b74637f97e59226121e88
KFcghost <66289994+KFcghost@users.noreply.github.com>
2021-07-31 17:22:30 +0800

    12

--- NC.cpp
+++
@@ -1,7 +0,0 @@
-/*
- * @Description: LEETCODE
- * @Author: Aspirin
- * @Date: 2021-07-28 23:21:04
- * @LastEditTime: 2021-07-28 23:34:17
- * @LastEditors: Aspirin
- */
---
+++ NC49最长括号字串.cpp
@@ -0,0 +1,24 @@
+/*
+ * @Description: LEETCODE
+ * @Author: Aspirin
+ * @Date: 2021-07-28 23:21:04
+ * @LastEditTime: 2021-07-31 17:22:22
+ * @LastEditors: Aspirin
+ */
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * 
+     * @param s string字符串 
+     * @return int整型
+     */
+    int longestValidParentheses(string s) {
+        // write code here
+        
+    }
+};
