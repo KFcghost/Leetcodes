c0b4b35d931c5ae55b71f43d2fc19dc8058dfe4d
KFcghost <66289994+KFcghost@users.noreply.github.com>
2021-10-04 21:05:09 +0800

    temp

--- LC212单词搜索 II.cpp
+++ LC212单词搜索 II.cpp
@@ -2,7 +2,7 @@
  * @Description: LEETCODE
  * @Author: Aspirin
  * @Date: 2021-09-16 21:10:03
- * @LastEditTime: 2021-10-04 18:56:28
+ * @LastEditTime: 2021-10-04 21:05:04
  * @LastEditors: Aspirin
  */
 #include <vector>
@@ -14,9 +14,34 @@
 #include <set>
 #include <queue>
 #include <deque>
+#include <string>
 
 using namespace std;
 
+struct Tirenode
+{
+    /* data */
+    string word;
+    unordered_map<char,Tirenode *> next;
+    Tirenode(){
+        this->word = "";
+    }
+};
+
+void insertword(Tirenode *root,const string word)
+{
+    Tirenode *newroot = root;
+
+    for(auto x : word)
+    {
+        if(!newroot->next.count(x))
+        {
+            newroot->next[x] = new Tirenode();
+        }
+        newroot = newroot->next[x];
+    }
+    newroot->word = word;
+}
 
 
 class Solution {
