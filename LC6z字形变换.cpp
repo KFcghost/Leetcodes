ecb3e14913523c744932e4bdba1af6877d029273
Aspirin <aspirink@qq.com>
2022-06-19 12:02:32 +0800

    6.19

---
+++ LC6z字形变换.cpp
@@ -0,0 +1,29 @@
+/*
+ * @Author: Aspirin
+ * @Date: 2022-06-11 20:29:24
+ * @LastEditTime: 2022-06-12 09:44:35
+ * @FilePath: /Leetcode/Leetcodes/6.z-字形变换.cpp
+ * @Description: self Leetcodes
+ * 
+ * Copyright (c) 2022 by $ Aspirin, All Rights Reserved. 
+ */
+/*
+ * @lc app=leetcode.cn id=6 lang=cpp
+ *
+ * [6] Z 字形变换
+ */
+
+// @lc code=start
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        
+    }
+};
+// @lc code=end
+
