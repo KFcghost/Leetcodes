ffc95987f32c879eb7cc400477ea61a1f4181c13
Aspirin <aspirink@qq.com>
2022-06-29 20:19:40 +0800

    6.29

---
+++ LC11盛最多水的容器.cpp
@@ -0,0 +1,29 @@
+/*
+ * @Author: Aspirin
+ * @Date: 2022-06-29 20:17:52
+ * @LastEditTime: 2022-06-29 20:18:22
+ * @FilePath: /Leetcode/Leetcodes/LC11盛最多水的容器.cpp
+ * @Description: self Leetcodes
+ * 
+ * Copyright (c) 2022 by $ Aspirin, All Rights Reserved. 
+ */
+/*
+ * @lc app=leetcode.cn id=11 lang=cpp
+ *
+ * [11] 盛最多水的容器
+ */
+
+// @lc code=start
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+
+    }
+};
+// @lc code=end
+
